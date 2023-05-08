73c22ee0e8de817e1a5590e0a8e0b23be2d2c732
Uriel Ortega <urielortega2509@gmail.com>
2023-05-08 09:25:12 -0600

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Uriel Ortega on 08/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
