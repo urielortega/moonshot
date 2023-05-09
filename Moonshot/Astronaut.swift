da21d1e6f3a76a8dd718d1c62226db86efc25098
Uriel Ortega <urielortega2509@gmail.com>
2023-05-09 12:19:16 -0600

    Added assets and loaded Codable data.

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Uriel Ortega on 09/05/23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Moonshot/Bundle-Decodable.swift
@@ -0,0 +1,28 @@
+//
+//  Bundle-Decodable.swift
+//  Moonshot
+//
+//  Created by Uriel Ortega on 09/05/23.
+//
+
+import Foundation
+
+extension Bundle {
+    func decode(_ file: String) -> [String: Astronaut] {
+        guard let url = self.url(forResource: file, withExtension: nil) else { // Locating the file in the Bundle.
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else { // Loading the data from the file URL.
+            fatalError("Failed to load \(file) from bundle.")
+        }
+        
+        let decoder = JSONDecoder()
+        
+        guard let loaded = try? decoder.decode([String: Astronaut].self, from: data) else { // Decoding the JSON file with Astronaut type.
+            fatalError("Failed to decode \(file) from bundle.")
+        }
+        
+        return loaded // Return the decoded data.
+    }
+}
